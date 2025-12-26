2e3b18455fd172a47b25875fc762c322e7d42252
FitApp Developer <dev@fitapp.com>
2025-12-26 12:39:32 +0200

    Use renamed logo file to force cache refresh

--- src/components/CustomHeader.tsx
+++ src/components/CustomHeader.tsx
@@ -146,7 +146,7 @@ const CustomHeader: React.FC<CustomHeaderProps> = ({ onFriendStreakPress }) => {
             </>
           ) : (
             <Image
-              source={require('../../assets/gym-branding/logo.png')}
+              source={require('../../assets/gym-branding/logo-new.png')}
               style={styles.topBarLogo}
               resizeMode="contain"
             />
